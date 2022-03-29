41bc81c4ecf3530f8554049bdb5d1f431dcd2824
SYH <406067937@qq.com>
2022-03-29 20:03:43 +0800

    ZinxV0.3

---
+++ myDemo/ZinxV0.3/server.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"zinx/zinx/ziface"
+	"zinx/zinx/znet"
+)
+
+/*
+	基于Zinx框架来开发的服务器端应用程序
+*/
+
+//ping test 自定义路由
+type PingRouter struct {
+	znet.BaseRouter
+}
+
+//Test PreHandle
+func (PR *PingRouter) PreHandle(request ziface.IRequest) {
+	fmt.Println("Call Router PreHandle...")
+	_, err := request.GetConnection().GetTCPConnection().Write([]byte("before ping...\n"))
+	if err != nil {
+		fmt.Println("call back before ping error")
+	}
+}
+
+//Test Handle
+func (PR *PingRouter) Handle(request ziface.IRequest) {
+	fmt.Println("Call Router Handle...")
+	_, err := request.GetConnection().GetTCPConnection().Write([]byte("ping...\n"))
+	if err != nil {
+		fmt.Println("call back ping... error")
+	}
+}
+
+//Test PostHandle
+func (PR *PingRouter) PostHandle(request ziface.IRequest) {
+	fmt.Println("Call Router PostHandle...")
+	_, err := request.GetConnection().GetTCPConnection().Write([]byte("after ping...\n"))
+	if err != nil {
+		fmt.Println("call back after ping error")
+	}
+}
+
+func main() {
+	//1 创建一个server句柄，使用Zinx的api
+	s := znet.NewServer("[zinx v0.3]")
+	//2 给当前zinx框架添加一个自定义router
+	s.AddRouter(&PingRouter{})
+	//3 启动server
+	s.Serve()
+}
